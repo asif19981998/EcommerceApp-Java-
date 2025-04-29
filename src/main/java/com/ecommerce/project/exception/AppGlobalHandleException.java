8a8fd1075984e1d001de5ac9fa914796a5d4fb78
asif19981998 <rakibulasif1998@gmail.com>
2025-04-29 19:20:03 +0600

    error handling add

--- src/main/java/com/ecommerce/project/exception/AppGlobalHandleException.java
+++ src/main/java/com/ecommerce/project/exception/AppGlobalHandleException.java
@@ -1,6 +1,27 @@
 package com.ecommerce.project.exception;
 
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class AppGlobalHandleException {
 
-    
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String,String> appMethodArgumentNotGivenException(MethodArgumentNotValidException e){
+        Map<String,String> response = new HashMap<String,String>();
+        e.getBindingResult().getAllErrors().forEach((err)->{
+            String fieldName = ((FieldError)err).getField();
+            String message = err.getDefaultMessage();
+            response.put(fieldName,message);
+        });
+        return  response;
+    }
+
 }
