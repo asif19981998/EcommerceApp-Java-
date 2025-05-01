f882d4962fda82a469ed6ea65b6b1cbacf3564ec
rakibul19981998 <rakibulasif1998@gmail.com>
2025-05-01 19:24:49 +0600

    sorting add, exception model add

--- src/main/java/com/ecommerce/project/exception/AppGlobalHandleException.java
+++ src/main/java/com/ecommerce/project/exception/AppGlobalHandleException.java
@@ -1,5 +1,6 @@
 package com.ecommerce.project.exception;
 
+import com.ecommerce.project.payload.APIResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -28,15 +29,17 @@ public class AppGlobalHandleException {
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<String> appResourceNotFoundException(ResourceNotFoundException e){
+    public ResponseEntity<APIResponse> appResourceNotFoundException(ResourceNotFoundException e){
         String message = e.getMessage();
-        return new ResponseEntity<>(message,HttpStatus.NOT_FOUND);
+        APIResponse apiResponse = new APIResponse(message,false);
+        return new ResponseEntity<>(apiResponse,HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(APIException.class)
-    public ResponseEntity<String> appAPIException(APIException e){
+    public ResponseEntity<APIResponse> appAPIException(APIException e){
         String message = e.getMessage();
-        return new ResponseEntity<>(message,HttpStatus.BAD_REQUEST);
+        APIResponse apiResponse = new APIResponse(message,false);
+        return new ResponseEntity<>(apiResponse,HttpStatus.BAD_REQUEST);
     }
 
 }
