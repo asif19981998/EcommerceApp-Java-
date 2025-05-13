94c954f476795163cb6a493d8b9545564fe9429d
rakibul19981998 <rakibulasif1998@gmail.com>
2025-05-13 23:41:21 +0600

    authentication entity add

---
+++ src/main/java/com/ecommerce/project/model/User.java
@@ -0,0 +1,32 @@
+package com.ecommerce.project.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @NotBlank
+    @Size(max = 20)
+    private String userName;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(max = 120)
+    private String password;
+}
