3b41b2b00a2e7f330d3ea5ba19743c76474cbe63
asif19981998 <rakibulasif1998@gmail.com>
2025-05-18 14:23:02 +0600

    add userrepo,services

--- src/main/java/com/ecommerce/project/security/services/UserDetailsImpl.java
+++ src/main/java/com/ecommerce/project/security/services/UserDetailsImpl.java
@@ -1,13 +1,17 @@
 package com.ecommerce.project.security.services;
 
+import com.ecommerce.project.model.User;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @NoArgsConstructor
 @Data
@@ -23,38 +27,74 @@ public class UserDetailsImpl implements UserDetails {
 
     private Collection<? extends GrantedAuthority> authorities;
 
+    public UserDetailsImpl(Long id, Collection<? extends GrantedAuthority> authorities, String password, String email, String username) {
+        this.id = id;
+        this.authorities = authorities;
+        this.password = password;
+        this.email = email;
+        this.username = username;
+    }
+
+    public static UserDetailsImpl build(User user){
+        List<GrantedAuthority>  authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getRoleName().name()))
+                .collect(Collectors.toList());
+
+
+        return new UserDetailsImpl(
+                user.getUserId(),
+                authorities,
+                user.getEmail(),
+                user.getPassword(),
+                user.getUserName()
+        );
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return authorities;
     }
 
     @Override
     public String getPassword() {
-        return "";
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return "";
+        return username;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return UserDetails.super.isAccountNonExpired();
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return UserDetails.super.isAccountNonLocked();
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return UserDetails.super.isCredentialsNonExpired();
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return UserDetails.super.isEnabled();
+        return true;
     }
+
+    @Override
+    public boolean equals(Object o){
+        if(this == o){
+            return true;
+        }
+        if(o == null || getClass() != o.getClass()){
+            return false;
+        }
+        UserDetailsImpl user = (UserDetailsImpl) o;
+        return Objects.equals(id,user.id);
+    }
+
 }
