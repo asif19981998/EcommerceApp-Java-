2f19547d894dbb6e0a293995be25d8926d3aaee0
asif19981998 <rakibulasif1998@gmail.com>
2025-06-03 16:38:07 +0600

    cart item service add

--- src/main/java/com/ecommerce/project/repositories/CartItemRepository.java
+++ src/main/java/com/ecommerce/project/repositories/CartItemRepository.java
@@ -1,4 +1,11 @@
 package com.ecommerce.project.repositories;
 
-public interface CartItemRepository {
+import com.ecommerce.project.model.Cart;
+import com.ecommerce.project.model.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface CartItemRepository extends JpaRepository<CartItem,Long> {
+    @Query("SELECT c FROM CartItem c WHERE ci.cart.id = ?1 AND ci.product.id = ?2")
+    CartItem findCartItemByProductIdAndCartId(Long cardId, Long productId);
 }
--- src/main/java/com/ecommerce/project/repositories/CartRepository.java
+++ src/main/java/com/ecommerce/project/repositories/CartRepository.java
@@ -1,4 +1,10 @@
 package com.ecommerce.project.repositories;
 
-public interface CartRepository {
+import com.ecommerce.project.model.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface CartRepository extends JpaRepository<Cart,Long> {
+    @Query("SELECT c FROM Cart c WHERE C.user.email = ?1")
+    Cart findCartByEmail(String email);
 }
