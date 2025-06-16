7807cf5ceb994478fa3181bec941c17e6b5a9846
asif19981998 <rakibulasif1998@gmail.com>
2025-06-16 13:03:57 +0600

    get all cart api end create

--- src/main/java/com/ecommerce/project/Controller/CartController.java
+++ src/main/java/com/ecommerce/project/Controller/CartController.java
@@ -8,10 +8,9 @@ import org.hibernate.Internal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -25,5 +24,11 @@ public class CartController {
                                                     @PathVariable Integer quantity){
         CartDTO cartDTO = cartService.addProductToCart(productId,quantity);
         return new ResponseEntity<CartDTO>(cartDTO, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/carts")
+    public ResponseEntity<List<CartDTO>> getCarts(){
+        List<CartDTO> cartDTOS = cartService.getAllCarts();
+        return new ResponseEntity<List<CartDTO>>(cartDTOS,HttpStatus.FOUND);
     }
 }
